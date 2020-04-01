dca18eb1831c4bbdff6ee9e0545b83c777a12349
hdehal <harry@hdehal.com>
2020-04-01 20:15:41 +0000

    Add map and dummy data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header';
+import CoffeeMap from './components/map.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <CoffeeMap />
     </div>
   );
 }
---
+++ src/components/map.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Map, CircleMarker, TileLayer, Tooltip, AttributionControl } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import data from "./mapData"
+
+/* Adapted from https://github.com/afzalsayed96/bubbles-map by Afzal Sayed  */
+/* Map tiles proudly from Stamen Design in San Francisco https://stamen.com/maps/ */
+/* Additional help from http://leaflet-extras.github.io/leaflet-providers/preview/ */
+
+class CoffeeMap extends Component {
+
+  render() {
+    var centerLat = (data.minLat + data.maxLat) / 2;
+    var distanceLat = data.maxLat - data.minLat;
+    var bufferLat = distanceLat * 0.05;
+    var centerLong = (data.minLong + data.maxLong) / 2;
+    var distanceLong = data.maxLong - data.minLong;
+    var bufferLong = distanceLong * 0.05;
+    return (
+      <div>
+        <Map
+          style={{ height: "480px", width: "100%", opacity: "0.9" }}
+          zoom={1}
+          center={[centerLat, centerLong]}
+          bounds={[
+            [data.minLat - bufferLat, data.minLong - bufferLong],
+            [data.maxLat + bufferLat, data.maxLong + bufferLong]
+          ]}
+          attributionControl={false}>
+          <TileLayer url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png" 
+          attribution="Map by <a href='http://stamen.com' target='_blank'>Stamen Design</a> | Data &copy; <a href='https://www.openstreetmap.org/copyright' target='_blank'>OpenStreetMap</a> contributors"
+          />
+
+        <AttributionControl position="bottomright" prefix={false} />
+
+          {data.city.map((city, k) => {
+            return (
+              <CircleMarker
+                key={k}
+                center={[city["coordinates"][1], city["coordinates"][0]]}
+                radius={20 * Math.log(city["population"] / 10000000)}
+                fillOpacity={0.5}
+                stroke={false}>
+                <Tooltip direction="right" offset={[-8, -2]} opacity={1}>
+                    {/* Fix Unexpected string concatenation of literals  no-useless-concat error */}
+                    {/* <span>{city["name"] + ": " + "Population" + " " + city["population"]}</span> */}
+                    <span>`${city["name"]|"Population"|city["population"]}`</span>
+                </Tooltip>
+              </CircleMarker>)
+          })
+          }
+        </Map>
+      </div>
+    );
+  }
+}
+
+export default CoffeeMap;
