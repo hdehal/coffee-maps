3f6dbec59b239dda912fc1eacd815884da2e4c69
hdehal <harry@hdehal.com>
2020-04-08 22:38:01 +0000

    Updates

---
+++ precache-manifest.9ee9e45006891ffb45696b51029e6f94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b823a30c92a75826f3d1c09626993a1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5664aa6527051e9ce7c3",
+    "url": "/static/css/2.fb66d78c.chunk.css"
+  },
+  {
+    "revision": "5645ac4ea2709f028814",
+    "url": "/static/css/main.7e6cc15b.chunk.css"
+  },
+  {
+    "revision": "5664aa6527051e9ce7c3",
+    "url": "/static/js/2.2b31845f.chunk.js"
+  },
+  {
+    "revision": "70eb0bc123cf70678e8504782a29f363",
+    "url": "/static/js/2.2b31845f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5645ac4ea2709f028814",
+    "url": "/static/js/main.a0c4995b.chunk.js"
+  },
+  {
+    "revision": "287faf106a4db4297aa4",
+    "url": "/static/js/runtime-main.33d04056.js"
+  },
+  {
+    "revision": "5c039dac00fb21a8cdb9252e8b356b0f",
+    "url": "/static/media/bean.5c039dac.svg"
+  }
+]);
