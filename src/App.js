dd101b73919e787ab71efbdff8e88194f02dc33d
hdehal <harry@hdehal.com>
2020-04-09 17:38:41 +0000

    Clean-up and convert function to class

--- src/App.js
+++ src/App.js
@@ -1,42 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header';
 import CoffeeMap from './components/map.js';
 import CoffeeTable from './components/table';
 
 // Material-UI
-import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-}));
+class App extends Component {
 
-function App() {
-
-  const classes = useStyles();
-
-  return (
-    <div className="App">
-      <Header />
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={8}>
-          <Paper className={classes.paper}>
-            <CoffeeMap />
-          </Paper>
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={8}>
+            <Paper>
+              <CoffeeMap />
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Paper>
+              <CoffeeTable />
+            </Paper>
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper}>
-            <CoffeeTable />
-          </Paper>
-        </Grid>
-      </Grid>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
