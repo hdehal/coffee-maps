1c6d96add4c1b681fb9af0c581a37b39eaa062b2
hdehal <harry@hdehal.com>
2020-08-25 06:30:23 +0000

    Updates

---
+++ precache-manifest.db6a932c1279272862d92ace32ab4ce3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c085b74b0491b8d9c50f7c5c28dcac30",
+    "url": "/coffee-maps/index.html"
+  },
+  {
+    "revision": "89454f28fe62032bbf7b",
+    "url": "/coffee-maps/static/css/2.fb66d78c.chunk.css"
+  },
+  {
+    "revision": "512beb16ababbde66d14",
+    "url": "/coffee-maps/static/css/main.53cb2a35.chunk.css"
+  },
+  {
+    "revision": "89454f28fe62032bbf7b",
+    "url": "/coffee-maps/static/js/2.13015dc1.chunk.js"
+  },
+  {
+    "revision": "b747d1fb9784f29a328986ba4ad6eb84",
+    "url": "/coffee-maps/static/js/2.13015dc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "512beb16ababbde66d14",
+    "url": "/coffee-maps/static/js/main.f6c288df.chunk.js"
+  },
+  {
+    "revision": "125f18b609e5bfa82679",
+    "url": "/coffee-maps/static/js/runtime-main.b37f9257.js"
+  },
+  {
+    "revision": "5c039dac00fb21a8cdb9252e8b356b0f",
+    "url": "/coffee-maps/static/media/bean.5c039dac.svg"
+  }
+]);
